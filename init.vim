f7e7b8396b721b751faa296231c7126a68931415
jakulej <j.kulej01@gmail.com>
2024-07-25 09:21:02 +0200

    first commit

---
+++ init.vim
@@ -0,0 +1,81 @@
+"Tab settings
+set tabstop=4
+set shiftwidth=4
+set expandtab
+
+
+" Space as Mapleader
+let mapleader = " "
+
+" Open NetRw on Space+b
+nnoremap <leader>b :Ex<CR>
+" Close on Space+q
+nnoremap <leader>q :q<CR>
+" Write on Space+w
+nnoremap <leader>w :w<CR>
+
+" Map Space+h/l to move between tabs
+nnoremap <leader>h gT
+nnoremap <leader>l gt
+
+" Big jump on Space+j/k
+nnoremap <leader>j <C-f>
+nnoremap <leader>k <C-b>
+
+" Open new tab on Space+n
+nnoremap <leader>n :tabnew<CR>:Ex<CR>
+
+" Terminal Actions
+nnoremap <leader>tc :terminal<CR>i
+nnoremap <leader>tv <C-w>v<C-w>l:terminal<CR>i
+nnoremap <leader>ts <C-w>s<C-w>j:terminal<CR>i
+
+" Do not copy the deleted text (Visual mode)
+vnoremap d "_d
+vnoremap D "_D
+" Do not copy the deleted text (Normal mode)
+nnoremap d "_d
+nnoremap D "_D
+nnoremap dd "_dd
+
+" Always paste from clipboard (Visual mode)
+vnoremap p "+P
+vnoremap P "+p
+" Always paste from clipboard (Normal mode)
+nnoremap p "+p
+nnoremap P "+P
+
+" Always copy to clipboard (Visual mode)
+vnoremap y "+y
+vnoremap x "+x
+
+" Do not end selection after adding/removing tabs
+vnoremap > >gv
+vnoremap <lt> <lt>gv
+
+" Do not enter INSERT mode after adding newline
+nnoremap o o<Esc>
+nnoremap O O<Esc>
+
+" Nice line deletions
+nnoremap K kJ
+
+" !!! This is necessary to exit the terminal with Escape
+tnoremap <Esc> <C-\><C-n>
+
+" Some remaps for Escape (Easier Access)
+inoremap <C-c> <Esc>
+nnoremap <C-h> <Esc>
+inoremap <C-h> <Esc>
+vnoremap <C-h> <Esc>
+tnoremap <C-h> <C-\><C-n>
+cnoremap <C-h> <Esc>
+
+" Jumping to the start/end of the line
+nnoremap <leader>a g_
+nnoremap <leader>i _
+vnoremap <leader>a g_
+vnoremap <leader>i _
+
+lua require('plugins')
+
---
+++ lua/plugins.lua
@@ -0,0 +1,173 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+print(lazypath)
+-- Example using a list of specs with the default options
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+
+require("lazy").setup({
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
+    "folke/which-key.nvim",
+    {'nvim-tree/nvim-web-devicons'},
+    { "folke/neoconf.nvim", cmd = "Neoconf" },
+    "folke/neodev.nvim",
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 } ,
+    {'glepnir/nerdicons.nvim', cmd = 'NerdIcons', config = function() require('nerdicons').setup({}) end},
+    {'nvim-lualine/lualine.nvim',dependencies = { 'nvim-tree/nvim-web-devicons' }
+}
+})
+
+vim.cmd.colorscheme "catppuccin"
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {},
+  handlers = {
+    lsp_zero.default_setup,
+    rust_analyzer = function()
+    require('lspconfig').rust_analyzer.setup({
+    on_attach = on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+})
+end,
+  },
+})
+
+require('lspconfig').rust_analyzer.setup({
+    on_attach = function(client, bufnr)
+        vim.lsp.inlay_hint.enable(bufnr)
+    end
+})
+
+require'nvim-web-devicons'.setup {
+ -- your personnal icons can go here (to override)
+ -- you can specify color or cterm_color instead of specifying both of them
+ -- DevIcon will be appended to `name`
+ override = {
+  zsh = {
+    icon = "",
+    color = "#428850",
+    cterm_color = "65",
+    name = "Zsh"
+  }
+ };
+ -- globally enable different highlight colors per icon (default to true)
+ -- if set to false all icons will have the default icon's color
+ color_icons = true;
+ -- globally enable default icons (default to false)
+ -- will get overriden by `get_icons` option
+ default = true;
+ -- globally enable "strict" selection of icons - icon will be looked up in
+ -- different tables, first by filename, and if not found by extension; this
+ -- prevents cases when file doesn't have any extension but still gets some icon
+ -- because its name happened to match some extension (default to false)
+ strict = true;
+ -- same as `override` but specifically for overrides by filename
+ -- takes effect when `strict` is true
+ override_by_filename = {
+  [".gitignore"] = {
+    icon = "",
+    color = "#f1502f",
+    name = "Gitignore"
+  }
+ };
+ -- same as `override` but specifically for overrides by extension
+ -- takes effect when `strict` is true
+ override_by_extension = {
+  ["log"] = {
+    icon = "",
+    color = "#81e043",
+    name = "Log"
+  }
+ };
+ -- same as `override` but specifically for operating system
+ -- takes effect when `strict` is true
+ override_by_operating_system = {
+  ["apple"] = {
+    icon = "",
+    color = "#A2AAAD",
+    cterm_color = "248",
+    name = "Apple",
+  },
+ };
+}
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
