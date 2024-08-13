5553dcff49b54c75561d21af324986371d932752
Jakub Kulejewski <j.kulej01@gmail.com>
2024-08-13 12:30:57 +0200

    feat: added Cattpucin nice themes

--- lua/config.lua
+++ lua/config.lua
@@ -1,3 +1,4 @@
 require("lazy_init")
 require("plugins")
 require("lsp_servers")
+require("catpuccin_theme")
--- lua/lazy_init.lua
+++ lua/lazy_init.lua
@@ -35,5 +35,4 @@ require("lazy").setup({
     {'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }},
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' }
 })
-vim.cmd.colorscheme "catppuccin"
 
